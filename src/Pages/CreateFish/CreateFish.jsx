9ea197dda941f84354f08a212b1e7d3764c6be4d
nguyenphucthienan111 <nguyenphucthienan111@gmail.com>
2024-11-01 16:23:01 +0700

    Update CreateFish.jsx

--- src/Pages/CreateFish/CreateFish.jsx
+++ src/Pages/CreateFish/CreateFish.jsx
@@ -17,7 +17,7 @@ const CreateFish = () => {
     weight: 0,
     dob: "",
     gender: "Male",
-    origin: "", // Changed to empty string instead of default value
+    origin: "",
     foodName: "",
     foodWeight: 0,
   });
@@ -69,7 +69,6 @@ const CreateFish = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // Validate origin is selected
     if (!fishData.origin) {
       toast.error("Please select an origin", { autoClose: 1500 });
       return;
