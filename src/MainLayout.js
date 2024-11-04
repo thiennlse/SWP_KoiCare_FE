4372049806c355bb6d206743073e0ecdd215fc48
nguyenphucthienan111 <nguyenphucthienan111@gmail.com>
2024-11-04 18:26:25 +0700

    create contactbuttons

--- src/MainLayout.js
+++ src/MainLayout.js
@@ -1,15 +1,19 @@
 import Header from "./Components/header/header";
 import Footer from "./Components/footer/footer";
-import { Outlet } from "react-router-dom";
+import ContactButtons from "./ContactButton/ContactButtons";
+import { Outlet, useLocation } from "react-router-dom";
 import "./MainLayout.css";
 
 const MainLayout = () => {
+  const location = useLocation();
+  const isAdminPage = location.pathname === "/admin";
   return (
     <div className="layout-container">
       <Header />
       <div className="main-content">
         <Outlet />
       </div>
+      {!isAdminPage && <ContactButtons />}
       <Footer />
     </div>
   );
--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -60,7 +60,6 @@ const HomePage = () => {
         <BlogSection blogs={blogData} />
       </div>
       <ScrollToTop />
-      <ContactButtons />
     </>
   );
 };
@@ -440,32 +439,6 @@ const ScrollToTop = () => {
     >
       <FaArrowUp />
     </button>
-  );
-};
-
-const ContactButtons = () => {
-  return (
-    <div className="contact-buttons">
-      <a
-        href="https://zalo.me/0778138889"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="contact-button zalo-button"
-      >
-        <SiZalo size={30} />
-      </a>
-      <a
-        href="https://www.facebook.com/messages/t/479832175208645"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="contact-button messenger-button"
-      >
-        <FaFacebookMessenger size={30} />
-      </a>
-      <a href="tel:0965015422" className="contact-button support-button">
-        <BiSupport size={30} />
-      </a>
-    </div>
   );
 };
 
