fa9ec531b699be20a0843047792de6685090719b
nguyenphucthienan111 <nguyenphucthienan111@gmail.com>
2024-11-05 14:42:04 +0700

    update modal's productdetail & productdetail

--- src/Pages/ManageAdmin/Modal/ProductDetails.jsx
+++ src/Pages/ManageAdmin/Modal/ProductDetails.jsx
@@ -1,38 +1,49 @@
 import React from "react";
-import "../Modal/Modal.css";
+import "./Modal.css";
 
 const ProductDetails = ({ product, onClose }) => {
   return (
-    <div className="product-details-admin">
-      <h2>Product Details</h2>
-      <p>
-        <strong>Name:</strong> {product.name}
-      </p>
-      <p>
-        <strong>Image:</strong>{" "}
-      </p>
-      <img src={product.image} alt={product.name} className="product-image" />
-      <p>
-        <strong>Cost:</strong> ${product.cost}
-      </p>
-      <p>
-        <strong>Description:</strong> {product.description}
-      </p>
-      <p>
-        <strong>Origin:</strong> {product.origin}
-      </p>
-      <p>
-        <strong>Productivity:</strong> {product.productivity}
-      </p>
-      <p>
-        <strong>Code:</strong> {product.code}
-      </p>
-      <p>
-        <strong>In Stock:</strong> {product.inStock}
-      </p>
-      <button className="btn-close-admin" onClick={onClose}>
-        Close
-      </button>
+    <div className="modal-content-wrapper">
+      <div className="modal-scroll-content">
+        <h2>Product Details</h2>
+        <div className="details-grid">
+          <div className="details-text">
+            <p>
+              <strong>Name:</strong> {product.name}
+            </p>
+            <p>
+              <strong>Cost:</strong> ${product.cost}
+            </p>
+            <p>
+              <strong>Description:</strong> {product.description}
+            </p>
+            <p>
+              <strong>Origin:</strong> {product.origin}
+            </p>
+            <p>
+              <strong>Productivity:</strong> {product.productivity}
+            </p>
+            <p>
+              <strong>Code:</strong> {product.code}
+            </p>
+            <p>
+              <strong>In Stock:</strong> {product.inStock}
+            </p>
+          </div>
+          <div className="details-image">
+            <img
+              src={product.image}
+              alt={product.name}
+              className="product-image"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="modal-footer">
+        <button className="btn-close-admin" onClick={onClose}>
+          Close
+        </button>
+      </div>
     </div>
   );
 };
